11ec1abf383923df043fbcda3833c41a23115cbe
Benjamin Floyd <benjamin.floyd@cyberark.com>
2020-04-21 17:53:50 +0000

    Updating some scripts

--- 000-prerequisites/001-initialize-docker.sh
+++ 000-prerequisites/001-initialize-docker.sh
@@ -3,17 +3,9 @@
 . ../config.sh
 
 set -x
-# Download seed fetcher from github
-git clone https://github.com/cyberark/kubernetes-conjur-deploy.git
-
-# Build seed-fetcher
-pushd kubernetes-conjur-deploy/build/seed-fetcher
-CONJUR_NAMESPACE_NAME=latest ./build.sh
-popd
+docker pull cyberark/dap-seedfetcher
 
 docker pull cyberark/conjur-cli:5
 mkdir -p ../cli5/root
-
-ssh -i $DAP_SSH_KEY core@$DAP_INTERNAL_IP "docker save $CONJUR_APPLIANCE_IMG | gzip -9 -" > ../$CONJUR_APPLIANCE_FILE
 
 docker load -i ../$CONJUR_APPLIANCE_FILE
--- 001-prepare-kubernetes/000-push-container-images.sh
+++ 001-prepare-kubernetes/000-push-container-images.sh
@@ -7,8 +7,5 @@ $(aws ecr get-login --no-include-email --region us-east-1)
 
 set -x
 
-docker tag seed-fetcher:latest $SEED_FETCHER_REPO
-docker push $SEED_FETCHER_REPO
-
 docker tag $CONJUR_APPLIANCE_IMG $CONJUR_APPLIANCE_REPO
 docker push $CONJUR_APPLIANCE_REPO
